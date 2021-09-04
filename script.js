b548e57b715fd2203955ab9c5d13a1472cd3b7b2
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-09-04 16:49:59 -0400

    City Search List

---
+++ script.js
@@ -0,0 +1,20 @@
+//City search list 
+function createCityList(citySearchList) {
+    $("#city-list").empty();
+  
+    var keys = Object.keys(citySearchList);
+    for (var i = 0; i < keys.length; i++) {
+      var cityListEntry = $("<button>");
+      cityListEntry.addClass("list-group-item list-group-item-action");
+  
+      var splitStr = keys[i].toLowerCase().split(" ");
+      for (var j = 0; j < splitStr.length; j++) {
+        splitStr[j] =
+          splitStr[j].charAt(0).toUpperCase() + splitStr[j].substring(1);
+      }
+      var titleCasedCity = splitStr.join(" ");
+      cityListEntry.text(titleCasedCity);
+  
+      $("#city-list").append(cityListEntry);
+    }
+  }
