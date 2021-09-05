462ad66b822cc85d01afd380073d08e762da9d03
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-09-04 20:58:06 -0400

    clear button,search button, local storage

--- script.js
+++ script.js
@@ -18,3 +18,63 @@ function createCityList(citySearchList) {
       $("#city-list").append(cityListEntry);
     }
   }
+  
+  //User input to search for a city display given city's weather data
+$("#city-list").on("click", "button", function(event) {
+    event.preventDefault();
+    var city = $(this).text();
+
+    populateCityWeather(city, citySearchList);
+
+    $("#current-weather").show();
+    $("#forecast-weather").show();
+  });
+
+//Clear the search history from the page
+function clearHistory(event){
+  event.preventDefault();
+  sCity=[];
+  localStorage.removeItem("citySearchList");
+  document.location.reload();
+  }
+
+//Click event to clear history 
+$("#clear-history").on("click",clearHistory);
+    //function to populate the city- pull from the weather API for current weather and forecast
+function populateCityWeather(city, citySearchList) {
+    createCityList(citySearchList);
+  
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=885e9149105e8901c9809ac018ce8658&q=" +
+      city;
+  
+    var queryURL2 =
+      "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=885e9149105e8901c9809ac018ce8658&q=" +
+      city;
+  
+    var latitude;
+  
+    var longitude;
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // Store all of the retrieved data inside of an object called "weather"
+      .then(function(weather) {
+        // Log the queryURL
+        console.log(queryURL);
+  
+        // Log the resulting object
+        console.log(weather);
+  
+        var nowMoment = moment();
+        // display date 
+        var displayMoment = $("<h3>");
+        $("#city-name").empty();
+        $("#city-name").append(
+          displayMoment.text("(" + nowMoment.format("M/D/YYYY") + ")")
+        );
+  
+        var cityName = $("<h3>").text(weather.name);
+        $("#city-name").prepend(cityName);
